7f9e8b8818203eda075d1ef62ca03bdadbee62dc
mustafahassan065 <mustafaprogrammer786@gmail.com>
2025-05-26 16:32:12 +0500

    Update API baseURL for deployed backend

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  //baseURL: 'http://localhost:9000/api', // backend URL
-  baseURL: 'https://4af047bf-3f87-407d-a16b-b1f02ba7699b-00-1hjhdns8hkv6.sisko.replit.dev/'
+  baseURL: 'http://localhost:9000/api', // backend URL
+  
 });
